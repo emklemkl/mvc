ec37852f272839fde4c88dcb625d8b03a8756b34
emklemkl <emil94_karlsson@outlook.com>
2023-03-30 15:19:38 +0200

    First commit kmom01

---
+++ src/Controller/LuckyControllerJson.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+
+use DateTime;
+use DateTimeZone;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyControllerJson {
+    #[Route("/api/lucky/number")]
+    public function jsonNumber(): Response
+    {
+        $number = random_int(0, 100);
+
+        $data = [
+            'lucky-number' => $number,
+            'lucky-message' => 'Hi there!',
+        ];
+        // Prettyprint
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        return $response;
+    }
+
+    #[Route("/api/quote")]
+    public function jsonQuote(): Response
+    {
+        date_default_timezone_get();
+        $dateToday = date("y-m-d");
+        $timestamp = time();
+        $time = new DateTime();
+        $time->setTimezone(new DateTimeZone('Europe/Stockholm'));
+        $time->setTimestamp($timestamp);
+        $responseGenerated = $time->format("H:i:s");
+        $number = random_int(1, 3);
+        $quotes = [
+            '1' => "Life is what happens when you're busy making other plans. -John Lennon",
+            '2' => "If life were predictable it would cease to be life, and be without flavor. -Eleanor Roosevelt",
+            '3' => "The way to get started is to quit talking and begin doing. -Walt Disney"
+        ];
+        $aQuote =  [
+            $number => $quotes[$number],
+            "date" => $dateToday,
+            "timestamp"=> $responseGenerated,
+        ];
+        // Prettyprint
+        $response = new JsonResponse($aQuote);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        return $response;
+    }
+}
