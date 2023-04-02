2b025f057efc266ced424fe4fee8962234613d93
emklemkl <emil94_karlsson@outlook.com>
2023-04-02 15:26:49 +0200

    Final kmom01 version

--- src/Controller/LuckyControllerJson.php
+++ src/Controller/LuckyControllerJson.php
@@ -6,7 +6,7 @@ use DateTime;
 use DateTimeZone;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
+// use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LuckyControllerJson
