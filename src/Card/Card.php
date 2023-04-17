f450c86586891f3eefd52e894f4e5b68af19dadd
emklemkl <emil94_karlsson@outlook.com>
2023-04-17 09:02:48 +0200

    Börjar på uppgiften i kmom02

---
+++ src/Card/Card.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Card;
+
+class Card
+{
+    protected $card_values = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+    
+    public function __construct()
+    {
+        // const
+    }
+
+    public function hearts()
+    {
+        
+    }
+}
---
+++ src/Card/DeckOfCards.php
@@ -0,0 +1,7 @@
+<?php
+namespace App\DeckOfCards;
+
+class DeckOfCards
+{
+    
+}
